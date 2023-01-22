25a695c9b6dded920ba2e521601806f2cc6a45d2
David Normandin <dvdnormans@hotmail.com>
2023-01-22 14:27:40 -0500

    (BMICalculator)  added and retrieved views ID

--- BMICalculator/app/src/main/java/com/example/bmicalculator/MainActivity.java
+++ BMICalculator/app/src/main/java/com/example/bmicalculator/MainActivity.java
@@ -3,6 +3,12 @@ package com.example.bmicalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //
+        TextView resultTxtView = findViewById(R.id.txt_view_result);
+        Button calculateBtn = findViewById(R.id.btn_calculate);
+        EditText weightEditTxt = findViewById(R.id.edit_txt_weight);
+        EditText inchesEditTxt = findViewById(R.id.edit_txt_inches);
+        EditText feetEditTxt = findViewById(R.id.edit_txt_feet);
+        EditText ageEditTxt = findViewById(R.id.edit_txt_age);
+        RadioButton femaleRadioBtn = findViewById(R.id.radio_btn_female);
+        RadioButton maleRadioBtn = findViewById(R.id.radio_btn_male);
+        //
+        calculateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,
+                               "calculateBtn works!",
+                                Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
