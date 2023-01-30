1ec40c6b6911f0a827dac4ed1de5a07f1aabeeba
David Normandin <dvdnormans@hotmail.com>
2023-01-30 18:57:52 -0500

    (Labo1Spinners) ready to implement glide library

--- Labo1Spinners/app/src/main/java/com/example/labo1spinners/MainActivity.java
+++ Labo1Spinners/app/src/main/java/com/example/labo1spinners/MainActivity.java
@@ -2,13 +2,112 @@ package com.example.labo1spinners;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String[] VILLES_QUEBEC = {"Montréal", "Québec", "Laval", "Longueuil"};
+    private final String[] VILLES_ONTARIO = {"Toronto", "Ottawa", "Mississauga", "Brampton"};
+    private final String[] VILLES_CB = {"Vancouver", "Surrey", "Burnaby", "Richmond"};
+
+    private Spinner tableProvinces;
+    private Spinner tableVilles;
+    private ImageView imageProvince;
+
+    private List<String> provinces;
+    private String provinceChoisie;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setViews();
+        setTableProvinces();
     }
+
+    private void setImageProvince() {
+        switch (provinceChoisie) {
+            case "Québec":
+                imageProvince.setImageResource(R.drawable.quebec);
+                break;
+            case "Ontario":
+                imageProvince.setImageResource(R.drawable.ontario);
+                break;
+            case "Colombie-Britannique":
+                imageProvince.setImageResource(R.drawable.bc);
+                break;
+            default:
+                System.out.println("Oops");
+        }
+    }
+
+    private void setTableVilles() {
+        ArrayAdapter<String> dataAdapter;
+        switch (provinceChoisie) {
+            case "Québec":
+                dataAdapter = new ArrayAdapter<>(this,
+                        android.R.layout.simple_spinner_item,
+                        VILLES_QUEBEC);
+                dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                tableVilles.setAdapter(dataAdapter);
+                break;
+            case "Ontario":
+                dataAdapter = new ArrayAdapter<>(this,
+                        android.R.layout.simple_spinner_item,
+                        VILLES_ONTARIO);
+                dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                tableVilles.setAdapter(dataAdapter);
+                break;
+            case "Colombie-Britannique":
+                dataAdapter = new ArrayAdapter<>(this,
+                        android.R.layout.simple_spinner_item,
+                        VILLES_CB);
+                dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                tableVilles.setAdapter(dataAdapter);
+                break;
+            default:
+                System.out.println("Oops");
+        }
+    }
+
+    private void setTableProvinces() {
+        provinces = new ArrayList<>(Arrays.asList(getResources().getStringArray(R.array.table_provinces)));
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item,
+                provinces);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        tableProvinces.setAdapter(dataAdapter);
+        //
+        tableProvinces.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+                //arg0.setSelection(1);
+                provinceChoisie = (String) tableProvinces.getSelectedItem();
+                setTableVilles();
+                setImageProvince();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
+                // TODO Auto-generated method stub
+            }
+        });
+    }
+
+    private void setViews() {
+        tableProvinces = findViewById(R.id.table_provinces);
+        tableVilles = findViewById(R.id.table_villes);
+        imageProvince = findViewById(R.id.flag_imgview);
+    }
+
 }
