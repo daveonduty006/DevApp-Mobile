ac660fd8453cfbe04b59652225c6d34b87133f7e
David Normandin <dvdnormans@hotmail.com>
2023-03-12 01:02:18 -0500

    (ProjetExercices) added

---
+++ ProjetExercices/app/src/main/java/com/example/projetexercices/Exercices.java
@@ -0,0 +1,94 @@
+package com.example.projetexercices;
+
+public class Exercices {
+
+    private int id;
+    private String nom;
+    private String categorie;
+    private String description;
+    private String instructions;
+    private String urlVideo;
+    private String cheminImage;
+
+    public Exercices() {
+    }
+
+    public Exercices(int id, String nom, String categorie, String description, String instructions, String urlVideo, String cheminImage) {
+        this.id = id;
+        this.nom = nom;
+        this.categorie = categorie;
+        this.description = description;
+        this.instructions = instructions;
+        this.urlVideo = urlVideo;
+        this.cheminImage = cheminImage;
+    }
+
+    @Override
+    public String toString() {
+        return "Exercices{" +
+                "id=" + id +
+                ", nom='" + nom + '\'' +
+                ", categorie='" + categorie + '\'' +
+                ", description='" + description + '\'' +
+                ", instructions='" + instructions + '\'' +
+                ", urlVideo='" + urlVideo + '\'' +
+                ", cheminImage='" + cheminImage + '\'' +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getCategorie() {
+        return categorie;
+    }
+
+    public void setCategorie(String categorie) {
+        this.categorie = categorie;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getInstructions() {
+        return instructions;
+    }
+
+    public void setInstructions(String instructions) {
+        this.instructions = instructions;
+    }
+
+    public String getUrlVideo() {
+        return urlVideo;
+    }
+
+    public void setUrlVideo(String urlVideo) {
+        this.urlVideo = urlVideo;
+    }
+
+    public String getCheminImage() {
+        return cheminImage;
+    }
+
+    public void setCheminImage(String cheminImage) {
+        this.cheminImage = cheminImage;
+    }
+}
---
+++ ProjetExercices/app/src/main/java/com/example/projetexercices/MyDatabaseHelper.java
@@ -0,0 +1,24 @@
+package com.example.projetexercices;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class MyDatabaseHelper extends SQLiteOpenHelper {
+
+    public MyDatabaseHelper(@Nullable Context context) {
+        super(context, "exercices.db", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
